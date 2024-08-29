f1ffa36ef6fcb49ff0fb4ad9fb612d40d97716b8
Akshat Jaimini <destrex271@gmail.com>
2024-08-29 09:47:15 +0530

    Main dashboard ready

--- src/frontend/src/app/page.js
+++
@@ -1,8 +0,0 @@
-import { Component } from "@/components/component";
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <Component/>
-  );
-}
--- src/frontend/src/lib/apis.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export async function getAPISData() {
-    // Instead of the file system,
-    // fetch post data from an external API endpoint
-    const res = await axios.get('.http://localhost:6555/apis');
-    return res.json();
-  }
-  
